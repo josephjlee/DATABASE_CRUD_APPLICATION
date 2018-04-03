e4cab36c91524e08e9eb734677336a6a186d009a
Kevin KANON <34927281+kevinkanon@users.noreply.github.com>
2018-04-03 06:00:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,15 @@ require_once 'core/init.php';
 
 //echo Config::get('mysql/host'); // '127.0.0.1'
 
-//$user = DB::getInstance()->get('users', array('username', '=', 'alex'));
+
 //$user = DB::getInstance()->query("SELECT * FROM users");
-$user = DB::getInstance()->update('users', 3,  array(
+/*$user = DB::getInstance()->update('users', 3,  array(
     'password'  => 'newpassword',
     'name' => 'Zagalo Mendosa'
 ));
+*/
 
+$user = DB::getInstance()->get('users', array('username', '=', 'alex'));
 
 if(!$user->count())
 {
